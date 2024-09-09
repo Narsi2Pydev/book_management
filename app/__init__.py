6235f21206a35c4cbf5b298f453b49086facb423
Durga Narasimha Rao Gadi <57086727+Narsi2Pydev@users.noreply.github.com>
2024-09-09 16:35:57 +0530

    removed files

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from .config import Config
 from .database import init_db
 from routes.books import books_bp
 from routes.users import users_bp
+from routes.reviews import reviews_bp
 from routes.auth import auth_bp
 
 def create_app():
@@ -13,6 +14,7 @@ def create_app():
 
     app.register_blueprint(books_bp, url_prefix='/books')
     app.register_blueprint(users_bp, url_prefix='/users')
+    app.register_blueprint(reviews_bp, url_prefix='/reviews')
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
     return app
